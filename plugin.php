63791408600d64a2e063cf19b8e5048476b6e6d5
Jared.K <3268576+jaredkotoff@users.noreply.github.com>
2023-10-30 20:30:25 -0700

    init

---
+++ plugin.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Referral Parameter
+Plugin URI: https://github.com/jaredkotoff/yourls-referral-param
+Description: Track clicks based on url parameter
+Version: 1.0
+Author: Jared.K
+Author URI: https://jared.land
+ */
+
+// No direct call
+if (!defined('YOURLS_ABSPATH')) {
+    die();
+}
+
+/*
+
+Your code goes here.
+
+Suggested read:
+https://docs.yourls.org/guide/extend/plugins.html
+https://docs.yourls.org/development/plugins.html
+https://docs.yourls.org/development/coding-standards.html
+and in general the whole "Development" section on https://docs.yourls.org/
+
+Have fun!
+
+ */
